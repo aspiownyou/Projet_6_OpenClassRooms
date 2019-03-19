35028bcf38254eb391d8a2cf970e5754ab3137a5
darkan951 <47302016+darkan951@users.noreply.github.com>
2019-03-19 16:01:48 +0100

    correction test multi carte

--- RedHatScript.py
+++ RedHatScript.py
@@ -6,16 +6,16 @@ def carteE(carte):
                 x = "3"
                 chemin = "/etc/sysconfig/network-scripts/ifcfg-enp0s" + x
                 fichier = open(chemin, "w")
-                fichier.write("TYPE=" + carte["type"] + "\nBOOTPROTO=" + carte["mode"] + "\nONBOOT=yes \nIPADDR=" + carte["adresse"] + "\nNETMASK" + carte["netmask"])
+                fichier.write("TYPE=" + carte["type"] + "\nBOOTPROTO=" + carte["mode"] + "\nONBOOT=yes \nIPADDR=" + carte["adresse"] + "\nNETMASK=" + carte["netmask"])
                 print("le fichier "+chemin+' à été créé')
         if carte['num'] > 1:
                 x = carte['num'] + 6
                 chemin = "/etc/sysconfig/network-scripts/ifcfg-enp0s" + str(x)
                 fichier = open(chemin, "w")
-                fichier.write("TYPE=" + carte["type"] + "\nBOOTPROTO=" + carte["mode"] + "\nONBOOT=yes \nIPADDR=" + carte["adresse"] + "\nNETMASK" + carte["netmask"])
+                fichier.write("TYPE=" + carte["type"] + "\nBOOTPROTO=" + carte["mode"] + "\nONBOOT=yes \nIPADDR=" + carte["adresse"] + "\nNETMASK=" + carte["netmask"])
                 print("le fichier "+chemin+' à été créé')
         else:
-                print("Impossible de créer un fichier de configuration, le fichier YAML doit etre mal renseigné.")
+                print("probleme YAML")
 
 
 def dhcp():
