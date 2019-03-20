6ea757317cacc2882c5f552326c39107846c556f
darkan951 <47302016+darkan951@users.noreply.github.com>
2019-03-20 16:18:49 +0100

    correction du remplissage du fichier de conf

--- DebianScript.py
+++ DebianScript.py
@@ -1,7 +1,7 @@
 # Script pour debian
 
 def carteE(carte, fichier):
-    fichier.write("# " + str(carte["num"]) + "interface \niface " + carte["name"] + " inet static:\n" + "    address " + carte["adresse"] + "\n    netmask " + carte["netmask"] + "\n    gateway " + carte["gateway"])
+    fichier.write("# " + str(carte["num"]) + "interface \niface " + carte["name"] + " inet static\n" + "    address " + carte["adresse"] + "\n    netmask " + carte["netmask"] + "\n    gateway " + carte["gateway"])
     print(fichier)
 
 def dhcp():
