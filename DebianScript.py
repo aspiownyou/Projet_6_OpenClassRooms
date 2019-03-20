af2992f68fd9106cab455fbc2b748737ba76cf3b
darkan951 <47302016+darkan951@users.noreply.github.com>
2019-03-20 16:49:09 +0100

    script fonctionnel
    l'ajout marche pour les cartes sur Opensuse, Fedora et Debian

--- DebianScript.py
+++ DebianScript.py
@@ -1,7 +1,7 @@
 # Script pour debian
 
 def carteE(carte, fichier):
-    fichier.write("\n \n# " + str(carte["num"]) + "interface \nauto " + carte["name"] + "\nallow-hotplug " + carte["name"] + "\niface " + carte["name"] + " inet static\n" + "    address " + carte["adresse"] + "\n    netmask " + carte["netmask"] + "\n    gateway " + carte["gateway"])
+    fichier.write("\n \n# " + str(carte["num"]) + "interface \nauto " + carte["name"] + "\nallow-hotplug " + carte["name"] + "\niface " + carte["name"] + " inet static\n" + "    address " + carte["adresse"] + "\n    netmask " + carte["netmask"] + "\n    gateway " + carte["gateway"] + "\n    broadcast " + carte["broadcast"] + "\n")
     print(fichier)
 
 def dhcp():
