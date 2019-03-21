84fee60da306c4a031301d35acb49ef18a62e201
darkan951 <47302016+darkan951@users.noreply.github.com>
2019-03-21 16:43:17 +0100

    test netplan

--- NetplanScript.py
+++ NetplanScript.py
@@ -1,11 +1,11 @@
 # Script pour les distributions utilisants netplan
 
 def carteE(carte, fichier):
-    fichier.write("        " + carte["name"] + ":\n")
-    fichier.write("            addresses: [ " + carte["adresse"] + "/" + carte["cidr"] + " ]\n")
-    fichier.write("            broadcast: " + carte["broadcast"] + "\n")
+    fichier.write("    " + carte["name"] + ":\n")
+    fichier.write("      addresses: [ " + carte["adresse"] + "/" + carte["cidr"] + " ]\n")
+    fichier.write("      broadcast: " + carte["broadcast"] + "\n")
     if carte["gateway"] != '' :
-        fichier.write("            gateway4: " + carte["gateway"] + "\n")
+        fichier.write("      gateway4: " + carte["gateway"] + "\n")
 
 def dhcp():
     print("en cour")
--- ScriptCarte.py
+++ ScriptCarte.py
@@ -23,8 +23,8 @@ def creationInterface():
     if os.path.exists("/etc/netplan/"):
         fichier = open("/etc/netplan/50-cloud-init.yaml", "w")
         fichier.write("network:\n")
-        fichier.write("    version2:\n")
-        fichier.write("    ethernets:\n")
+        fichier.write("  version2:\n")
+        fichier.write("  ethernets:\n")
     elif os.path.exists("/etc/network/") and os.path.exists("/etc/netplan/") == False:
         fichier = open("/etc/network/interfaces", "w")
         fichier.write("# The loopback network interface \nauto lo \niface lo inet loopback \n \n")
