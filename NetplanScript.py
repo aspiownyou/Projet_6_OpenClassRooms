6182a551065ddcf44ba6c9464ce5a9e96b110521
darkan951 <47302016+darkan951@users.noreply.github.com>
2019-03-21 15:57:21 +0100

    Update NetplanScript.py

--- NetplanScript.py
+++ NetplanScript.py
@@ -3,6 +3,7 @@
 def carteE(carte, fichier):
     fichier.write("        " + carte["name"] + ":\n")
     fichier.write("            addresses: [ " + carte["adresse"] + "/" + carte["cidr"] + " ]\n")
+    fichier.write("            broadcast: " + carte["broadcast"] + "\n")
     if carte["gateway"] != '' :
         fichier.write("            gateway4: " + carte["gateway"] + "\n")
 
