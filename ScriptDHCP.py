a22e14ae5827d8aef2669eb7443d7a55687eff72
darkan951 <47302016+darkan951@users.noreply.github.com>
2019-03-25 09:56:39 +0100

    Update ScriptDHCP.py

--- ScriptDHCP.py
+++ ScriptDHCP.py
@@ -1,6 +1,6 @@
 def configDHCP(dhcp, fichier):
     
-    fichier.write("subnet " + dhcp["subnet"] + " netmask " + dhcp["netmask"] " {\n")
+    fichier.write("subnet " + dhcp["subnet"] + " netmask " + dhcp["netmask"] + "{\n")
     fichier.write("  range " + dhcp["range_start"] + " " + dhcp["range_end"] + ";\n")
     fichier.write("  option broadcast-address " + dhcp["broadcast"] + ";\n")
     fichier.write("  option router " + dhcp["router"] + ";\n")
